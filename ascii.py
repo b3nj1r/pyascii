08510bc3d63f6f9206a035682da992f5263b5411
b3nj1r <47711426+b3nj1r@users.noreply.github.com>
2020-12-01 18:50:53 -0600

    fixed video writing error

--- ascii.py
+++ ascii.py
@@ -1,10 +1,9 @@
 import conv
 import setup
 import cv2
-
-
+import numpy as np
 # scale factor
-scale = 5
+scale = input("enter scale factor of the resulting files: ")
 # dimensions of a character in pixels
 psi = 15
 
@@ -18,36 +17,43 @@ count=0
 
 for i in setup.imgs:
     # print(i)
-    cv2.imwrite('./out/out_{}.jpg'.format(count),conv.convert(cv2.imread(i),scale,psi,pr,pg,pb))
+    cv2.imwrite('./out/out_{}.jpg'.format(count),conv.convert(cv2.imread(i),scale,psi,pr,pg,pb,cv2.imread(i).shape[1],cv2.imread(i).shape[1]))
     count+=1;
 
 count=0
 
 for i in setup.vids:
-    # video capture
+    # read source video
     cap = cv2.VideoCapture(i)
-
-    # output width and height
-    owi = int(cap.get(3) * (scale / 100) * psi)
-    ohi = int(cap.get(4) * (scale / 100) * psi)
+    # output width, output height
+    owi, ohi = int(cap.get(3) * (scale / 100) * psi), int(cap.get(4) * (scale / 100) * psi)
+    # initialize output video
+    out = cv2.VideoWriter('./out/out.mp4{}'.format(count), cv2.VideoWriter_fourcc(*'mp4v'), 32, (owi, ohi))
+    if not cap.isOpened():
+        print('read error')
+    i = 0
     length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-
-    # output file
-    out = cv2.VideoWriter('./out/out_{}.avi'.format(count),cv2.VideoWriter_fourcc(*'XVID'), 24, (owi, ohi))
-    frame_count=0
     percentage=0
+    frame_count=0
 
-    while(cap.isOpened()):
-        ret, frame = cap.read()
-        if ret:
-            out.write(conv.convert(frame,scale,psi,pr,pg,pb))
-            print('file | {}\n'.format(i))
-            print('#'*(percentage//2)+'.'*((100//2) - (percentage//2))
+    # frame handle
+    while cap.isOpened:
+        ret, frm = cap.read()
+        if not ret:
+            # conclude loop when video terminates
+            break
+        if frame_count%6==0:
+            frame = np.array(conv.convert(frm,scale,psi,pr,pg,pb,owi,ohi))
+        cv2.imshow('frame',frm)
+        out.write(frame)
+        print('file | {}\n'.format(i))
+        print('#'*(percentage//2)+'.'*((100//2) - (percentage//2)))
         frame_count+=1
         # print(frame_count)
         percentage = int((frame_count/length)*100)
-        if not ret:
+        if cv2.waitKey(1) == ord('q'):
             break
+
     # release if successful
     out.release()
     cap.release()
--- conv.py
+++ conv.py
@@ -5,31 +5,26 @@ from PIL import Image, ImageFont, ImageDraw
 # frame conversion algorithm
 print("conv.py name:", __name__)
 
-def convert(im, scale, psi, pr, pg, pb):
+def convert(im, scale, psi, pr, pg, pb,owi,ohi):
     # calculate image scale factor
-    dsize = int(im.shape[1] * scale / 100), int(im.shape[0] * scale / 100)
-    owi, ohi = dsize[0]*psi,dsize[1]*psi
+        dsize = int(im.shape[1] * scale / 100), int(im.shape[0] * scale / 100)
+        rdc = cv2.resize(im, dsize)
+        rdc = np.array(rdc).astype(np.uint8)
 
-    # resize image contents for faster compute
-    rdc = cv2.resize(im, dsize)
-    rdc = np.array(rdc).astype(np.uint8)
-
-    # initialize output frames
-    img = Image.fromarray(cv2.resize(rdc, (owi, ohi), interpolation=cv2.INTER_NEAREST))
-    dw = ImageDraw.Draw(img)
-
-    # compute here
-    y = 0
-    for r in rdc:
-        x = 0
-        for c in r:
-            # chose from a range of 16 characters using the luminance as index
-            xtext = ' .,:ilwnmWNM68@#'[int(pr * c[0] + pg * c[1] + pb * c[2]) // 16]
-            # np.array(c).clip(96,128)
-
-            # write text to image
-            dw.text((x, y), xtext, (int((c[0] // 2) ** 1.3), int((c[1] // 2) ** 1.4), int((c[2] // 2) ** 1.5)), ImageFont.truetype('lucida_typewriter_regular.ttf', psi))
-            x += psi
-        y += psi
-    return np.array(img)
-
+        # initialize output frames
+        img = Image.fromarray(cv2.resize(rdc, (owi, ohi), interpolation=cv2.INTER_NEAREST))
+        dw = ImageDraw.Draw(img)
+        # compute here
+        y = 0
+        for r in rdc:
+            # iterate rows
+            x = 0
+            for c in r:
+                # iterate columns
+                xtext = ' .,:ilwnmWNM68@#'[int(pr * c[0] + pg * c[1] + pb * c[2]) // 16]
+                # np.array(c).clip(96,128)
+                dw.text((x, y), xtext, (int((c[0] // 2) ** 1.3), int((c[1] // 2) ** 1.4), int((c[2] // 2) ** 1.5)),
+                        ImageFont.truetype('lucida_typewriter_regular.ttf', psi))
+                x += psi
+            y += psi
+        return (np.array(img))
